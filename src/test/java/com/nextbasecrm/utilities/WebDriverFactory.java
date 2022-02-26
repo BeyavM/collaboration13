f3cb07d0d2bfd04e2c48dd6b901d9708011b5aee
Mullajanov <seanmullajanov@gmail.com>
2022-02-26 10:37:54 -0800

    Web Driver Factory Utility has been created

--- src/test/java/com/nextbasecrm/utilities/WebDriverFactory.java
+++ src/test/java/com/nextbasecrm/utilities/WebDriverFactory.java
@@ -1,4 +1,28 @@
 package com.nextbasecrm.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class WebDriverFactory {
+
+    public static WebDriver getDriver(String browserType){
+        if (browserType.equalsIgnoreCase("Chrome")){
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+
+        }else if (browserType.equalsIgnoreCase("Firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }else{
+            System.out.println("Given browser type does not exist/or is currently not supported");
+            System.out.println("Driver = null");
+            return null;
+        }
+
+
+    }
+
+
 }
